656ceac60d357057c7a21703398cd55205d573e9
Md Rasel Khan <immdraselkhan@gmail.com>
2022-11-17 01:15:48 +0600

    updated

--- README.md
+++ README.md
@@ -1,63 +1,19 @@
-# [ExpressJS](https://expressjs.com/en/starter/installing.html)
+# Express Starter
 
-Express.js, or simply Express, is a back end web application framework for building RESTful APIs with Node.js, released as free and open-source software under the MIT License. It is designed for building web applications and APIs.
+Express Starter api server build with the [Express](https://expressjs.com/en/starter/installing.html) (NodeJS framework).
 
-## [CORS](https://expressjs.com/en/resources/middleware/cors.html)
+#### API Server - [URL](URL)
 
-CORS is a node.js package for providing a Connect/Express middleware that can be used to enable CORS with various options.
+#### Live Website - [URL](URL)
 
-### Initial setup
+## Other Resources
+I used the following technologies and tools.
 
-1. Create a directory, then open it in terminal
-2. Initialize the node package manager, run "npm init" then follow the steps or "npm init -y"
-3. Install express and cors, run "npm i express cors"
-4. Open "index.js" file in code editor and add the following lines
-```
-  // Require(import) the express
-  const express = require('express');
-  // Require(import) the cors
-  const cors = require('cors');
-  // Call the express in a variable
-  const app = express();
-  // Declare the port in a variable to run the application
-  const port = process.env.PORT ||8000;
+### `MongoDB Atlas`
+[Documentation Link](https://www.mongodb.com/docs)
 
-  // Enable cors
-  app.use(cors());
+### `jsonwebtoken`
+[Github Link](https://github.com/auth0/node-jsonwebtoken)
 
-  // Create an API(Get) to make sure application is running perfectly where we used two request(req) & response(res) parameters
-  app.get('/', (req, res) => {
-    res.send('Server is running :)');
-  });
-
-  // Finally listening the app in the port that we declared before
-  app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
-  });
-```
-5. To start the app, run "node index.js"
-6. Install nodemon [Docs](https://www.npmjs.com/package/nodemon), that will help us to automatically restart the node app when file changes in the directory are detected, run "npm install -g nodemon"
-7. Start the app, run "nodemon index.js"
-8. Then, load http://localhost:8000 in a browser to see the output
-
-### Add data to serve
-
-1. Add the API file in JSON format
-2. Add a path on the index.js file
-```
-  // Import(require) the API file
-  const api_data = require('./data/api_file.json');
-  // Create the API(Get) to serve
-  app.get('/path_name', (req, res) => {
-    res.send(api_data);
-  });
-
-  // If you need dynamic path
-  app.get('/path_name/:id', (req, res) => {
-    const id = req.params.id; // If needed to convet id to number, just use "parseInt(req.params.id)"
-    // Get the dynamic data, like this
-    const data = api_data.find(every_data => every_data.id === id) || {};
-    // Serve now
-    res.send(data);
-  });
-```
+### `dotenv`
+[Github Link](https://github.com/motdotla/dotenv)
--- index.js
+++ index.js
@@ -2,13 +2,159 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 8000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
 
+// Middlewares
 app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Server is running :)');
+// JWT verify
+const verifyJWT = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization?.split(' ')[1];
+    if (token && token !== 'null') {
+      jwt.verify(token, process.env.ACCESS_API_TOKEN, (error, decoded) => {
+        if (error) {
+          res.status(401).send({
+            success: false,
+            error: 'Unauthorized access, token invalid!',
+          });
+          return;
+        } else {
+          req.decoded = decoded;
+          next();
+        };
+      });
+    } else {
+      res.status(401).send({
+        success: false,
+        error: 'Unauthorized access, token not found!',
+      });
+      return;
+    };
+  } catch (error) {
+    console.error(error.name, error.message);
+    res.send({
+      success: false,
+      error: error.message,
+    });
+  };
+};
+
+// If MongoDB Atlast, use this server URL (Cluster)
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@practice-cluster.kfbhlaq.mongodb.net/?retryWrites=true&w=majority`;
+
+// If MongoDB Compass, use this server URL
+const uri = 'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.0';
+
+// Creating a new client
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// Connecting the client to the cluster then creating database
+const dbConnect = async () => {
+  try {
+    // Connect client
+    await client.connect();
+
+    // Verify If connected, else bellow lines will not execute
+    console.log('Database is connected...');
+
+    // [USE IT TO TEST] Creating database and adding a collection then inserting a document
+    // const dbTesting = client.db('dbTest').collection('collectionTest');
+    // const result = await dbTesting.insertOne({'name': 'Md Rasel Khan'});
+    // console.log(result);
+
+  } catch (error) {
+    console.error(error.name, error.message);
+    res.send({
+      success: false,
+      error: error.message,
+    });
+  };
+};
+
+// Execute the above function
+dbConnect();
+
+/// CREATE DATABASES
+
+// Database name
+// const dbVariable = client.db('db_name');
+
+/// CREATE COLLECTIONS
+
+// Collection name
+// const collectionVariable = main.collection('collection_name');
+
+/// API ENDPOINTS
+
+// JWT
+app.post('/jwt', async (req, res) => {
+  try {
+    const user = req.body;
+    const token = jwt.sign(user, process.env.ACCESS_API_TOKEN, {expiresIn : '1d'});
+    if (user.userId) {
+      res.send({
+        success: true,
+        token,
+      });
+    } else {
+      res.send({
+        success: false,
+        error: 'Couldn\'t generate the token',
+      });
+    };
+  } catch (error) {
+    console.error(error.name, error.message);
+    res.send({
+      success: false,
+      error: error.message,
+    });
+  };
 });
 
+// JWT verification example
+app.get('/jwt', verifyJWT, async (req, res) => {
+  try {
+    const decoded = req.decoded;
+    if (decoded.userId !== req.params.userId) {
+      res.status(401).send({
+        success: false,
+        error: 'Unauthorized access, different user!',
+      });
+      return;
+    };
+    res.send({
+      success: true,
+      message: 'JWT verified',
+    });
+  } catch (error) {
+    console.log(error.name, error.message);
+    res.send({
+      success: false,
+      error: error.message,
+    });
+  };
+});
+
+// Verify the server is running or not
+app.get('/', (req, res) => {
+  try {
+    res.send({
+      success: true,
+      message: 'Server is running...',
+    });
+  } catch (error) {
+    console.log(error.name, error.message);
+    res.send({
+      success: false,
+      error: error.message,
+    });
+  };
+});
+
+// Listening the app on a particular port
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
